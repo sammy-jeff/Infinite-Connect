13f2484cc32a926eefdfbc0b76d41016e8ce8c92
sammy-jeff <okoyechibuike048@gmail.com>
2023-01-31 19:58:36 +0100

    modified firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -23,12 +23,12 @@ const firebaseConfig = {
 // ********************************
 // For development***********
 // const firebaseConfig = {
-//   apiKey: 'AIzaSyDHwPewsvmqxMv45AEpkwHVQetjrIGchzU',
-//   authDomain: 'linked-in-test.firebaseapp.com',
-//   projectId: 'linked-in-test',
-//   storageBucket: 'linked-in-test.appspot.com',
-//   messagingSenderId: '495861820872',
-//   appId: '1:495861820872:web:fa68766f833ab47027e4fd',
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID ,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID ,
 // }
 // ***********************************
 // Initialize Firebase
